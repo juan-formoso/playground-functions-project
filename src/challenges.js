364d32396133ee014465e5da572ac2a0d5b8b15f
Juan Formoso <trajano.juan@gmail.com>
2021-06-30 11:11:15 -0400

    Finalizando requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,8 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return ((wins * 3) + (ties * 1));
 }
 
 // Desafio 6
