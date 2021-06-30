5e92beac20c8d8fa88accccae9e6d75fab75b4d8
Juan Formoso <trajano.juan@gmail.com>
2021-06-30 00:58:05 -0400

    Finalizando requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
 
 // Desafio 4
